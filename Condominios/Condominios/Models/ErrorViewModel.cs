1fac17a7cafdcac5c255290494bf092e5d3d2a7b
Gustavo Canela <gustavochevtchuk@gmail.com>
2025-10-20 20:31:44 -0300

    startando projetos

---
+++ Condominios/Condominios/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Condominios.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
